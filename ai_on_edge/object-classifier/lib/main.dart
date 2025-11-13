8c70819fdb47f29e54a01c49fd95555c63131291
Yashas H Majmudar <yashashm.dev@gmail.com>
2025-11-13 10:33:34 -0500

    add: base UI setup

--- ai_on_edge/object-classifier/lib/main.dart
+++ ai_on_edge/object-classifier/lib/main.dart
@@ -9,10 +9,57 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: Text(
+            'Object Detection App',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          elevation: 10,
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(15),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              spacing: 20,
+              children: [
+                RichText(
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
+                    style: TextStyle(fontSize: 18, color: Colors.black),
+                    children: [
+                      TextSpan(
+                        text:
+                            'This is a simple demo of using tflite in your '
+                            'flutter app.The app uses the "',
+                      ),
+                      TextSpan(
+                        text: 'COCO (Common Objects in Context)',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      TextSpan(
+                        text:
+                            '" quantized model by google which detects 80 '
+                            'daily household objects.',
+                      ),
+                    ],
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Live Detection'),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Photo Detection'),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
